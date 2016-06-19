1277486df60cdbb99a6be44914891c88a844bbae
Ashi Agrawal <ashi.agrawal.2015@gmail.com>
2016-06-19 06:49:45 -0700

    made json handling code a bit cleaner

--- app/update_json.js
+++ app/update_json.js
@@ -1,12 +1,11 @@
 function findClusterByTab(clustersObj, tabId){
     var clusters = clustersObj.clusters;
-    for (i = 0; i < clusters.length; i ++){
+    for (var i = 0; i < clusters.length; i++){
         var start = clusters[i].start;
         if (tabId >= start) {
             var end = clusters[i].end;
             if (tabId <= end) return clusters[i].id;
         }
-
     }
     return null;
 }
@@ -15,13 +14,13 @@ function findClusterByTab(clustersObj, tabId){
 */
 function computeMostSimilarCluster(clusters, similarities){
     var highest = [0, 0];
-    for (i = 0; i < clusters.length; i ++){
+    for (var i = 0; i < clusters.length; i++){
         var sum = 0;
-        for (j = clusters[i].start; j <= clusters[i].end; j ++){
+        for (var j = clusters[i].start; j <= clusters[i].end; j++){
             sum += similarities[j];
         }
         var average = sum/(clusters[i].end - clusters[i].start + 1);
-	    if (average > highest[0]) highest = [average, i];
+	if (average > highest[0]) highest = [average, i];
     }
     return highest;
 }
@@ -31,7 +30,7 @@ function tabUpdated(clustersObj, similarities, activeTab){
     var highest = computeMostSimilarCluster(clustersObj.clusters,similarities);
     if (highest[0] > threshold){ //add new tab to existing cluster
         clustersObj.clusters[highest[1]].end ++;
-        for (i = 0; i < clustersObj.clusters.length; i ++) {
+        for (var i = 0; i < clustersObj.clusters.length; i++) {
             if (clustersObj.clusters[i].end > clustersObj.clusters[highest[1]].end) {
                 clustersObj.clusters[i].start ++;
 		        clustersObj.clusters[i].end ++;
@@ -41,6 +40,11 @@ function tabUpdated(clustersObj, similarities, activeTab){
         chrome.tabs.move(activeTab.id, {index: clustersObj.clusters[highest[1]].end}
     } else { //make a new cluster
         clustersObj['clusters'].push({"id": clustersObj.clusters.length, "start": similarities.length, "end": similarities.length});
+	chrome.tabs.move(activeTab.id, {index: similarities.length}
     }
     return clustersObj;
+}
+
+function tabRemoved(clustersObj, tabID) {
+    //TODO: Not yet implemented; var
 }
