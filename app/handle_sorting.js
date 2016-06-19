c7237d361bd0e83b62fdf90cbda70c2c06563b43
Vincent <vincentschen@yahoo.com>
2016-06-18 19:52:20 -0700

    retrieves HTML from current tab on page load

--- app/handle_sorting.js
+++ app/handle_sorting.js
@@ -1,8 +1,7 @@
-function sortTabs(prop){
-  /*
-   * Sort tabs based on tab property  
-   */  
-   
+/*
+ * Sort tabs based on tab property  
+ */  
+function sortTabs(prop){   
   prop = prop || "url";
   chrome.tabs.query({currentWindow: true}, function(tabs){
     tabs.sort(function(a,b){
@@ -14,11 +13,28 @@ function sortTabs(prop){
   });
 }
 
+/* 
+ * Executes a script that retrieves the page source
+ */ 
+function getPageSource() {
+  chrome.tabs.executeScript(
+    { 
+      code: "document.getElementsByTagName('html')[0].innerHTML;"
+    }, 
+    function (ps1) {
+      console.log(ps1);
+    }
+  );
+}
+
+/* 
+ * Listens for page load 
+ */ 
 chrome.tabs.onUpdated.addListener(function(tabId , info) {
-  // listens for page update event
   
+  // when page load completed
   if (info.status == "complete") {
-    // when page load completed
     sortTabs("url");
+    getPageSource();
   }
 });
