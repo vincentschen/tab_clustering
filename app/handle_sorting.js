f595f291626ce75b6b54a3aaf3ada28e00e20f53
Claire Shu <sclaire@stanford.edu>
2016-06-18 20:40:07 -0700

    working with favicons and title changes

--- app/handle_sorting.js
+++ app/handle_sorting.js
@@ -6,21 +6,12 @@ function sortTabs(prop){
     });
     tabs.forEach(function(tab){
       chrome.tabs.move(tab.id, {index: -1});
-      // chrome.tabs.update(tab.id, {}, function(tab) {
-      //   console.log("initial" + tab.favIconUrl);
-      //   tab.favIconUrl = "http://www.google.com/favicon.ico";
-      //   tab.title = "hello"; 
-      //   console.log("final" + tab.favIconUrl);
-      // });
       chrome.tabs.executeScript(tab.id,{code:"document.title = 'test'" });
-      //changeFavicon("url"); 
       chrome.tabs.executeScript(tab.id,{code:"document.querySelectorAll(\"link[rel*='mask-icon']\")[0].href = 'http://www.google.com/favicon.ico'"});
       chrome.tabs.executeScript(tab.id,{code:"document.querySelectorAll(\"link[rel*='shortcut icon']\")[0].href = 'http://www.google.com/favicon.ico'"});
       chrome.tabs.executeScript(tab.id,{code:"document.querySelectorAll(\"link[rel*='icon']\")[0].href = 'http://www.google.com/favicon.ico'"});
     
-
     });
-    //chrome.tabs.create({url:"http://www.google.com"});
   });
 }
 
@@ -29,15 +20,3 @@ chrome.tabs.onCreated.addListener(function(tab){
   sortTabs("url");
 
 });
-
-function changeFavicon(src) {
- var link = document.createElement('link'),
-     oldLink = document.getElementById('dynamic-favicon');
- link.id = 'http://www.google.com/favicon.ico';
- link.rel = 'http://www.google.com/favicon.ico';
- link.href = src;
- if (oldLink) {
-  document.head.removeChild(oldLink);
- }
- document.head.appendChild(link);
-}
