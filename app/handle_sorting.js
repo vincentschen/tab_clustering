9aee3f7b1a10c9ab4a6c00c352bed56f4f13c014
Claire Shu <sclaire@stanford.edu>
2016-06-19 01:06:26 -0700

    added change current tab

--- app/handle_sorting.js
+++ app/handle_sorting.js
@@ -58,9 +58,37 @@ chrome.tabs.onUpdated.addListener(function(tabId , info) {
   if (info.status == "complete") {
     sortTabs("url");
     getPageSource();
+    getCurrentTab("url"); 
   }
 });
 
-function changeCurrentTabIcon() {
+function getCurrentTab(prop) {
+  prop = prop || "url";
+  chrome.tabs.query({currentWindow: true}, function(tabs){
+    chrome.tabs.getSelected(null, function(tab){
+      var favicons = ["http://www.claireshu.com/favicon1.ico", 
+                      "http://www.claireshu.com/favicon2.ico", 
+                      "http://www.claireshu.com/favicon3.ico", 
+                      "http://www.claireshu.com/favicon4.ico", 
+                      "http://www.claireshu.com/favicon5.ico",
+                      "http://www.claireshu.com/favicon6.ico", 
+                      "http://www.claireshu.com/favicon7.ico", 
+                      "http://www.claireshu.com/favicon8.ico", 
+                      "http://www.claireshu.com/favicon9.ico", 
+                      "http://www.claireshu.com/favicon0.ico"];
 
+      var favicon = favicons[5];
+
+      var string1 = "document.title = 'zoom zoom'";
+      var string2 = "document.quuerySelectorAll(\"link[rel*='mask-icon']\")[0].href = '" + favicon + "'";
+      var string3 = "document.querySelectorAll(\"link[rel*='shortcut icon']\")[0].href = '" + favicon + "'";
+      var string4 = "document.querySelectorAll(\"link[rel*='icon']\")[0].href = '" + favicon + "'";
+      var vlongstring = string1 + ", " + string2 + ", " + string3; 
+
+      chrome.tabs.executeScript(tab.id,{code: vlongstring});
+      chrome.tabs.executeScript(tab.id,{code: string4});
+    });
+  });
 }
+
+
